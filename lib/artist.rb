2c2869eaf4467292f9775d1298d60f6e68130d33
Mykola Vasyl'yev <mykovasyl@gmail.com>
2022-07-07 17:08:15 -0400

    commit tree 2caad65447662fc556309be5624ad6897ce99460

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,13 +3,10 @@ require 'pry'
 class Artist
   attr_accessor :name
   attr_reader :songs
+  extend Memorable::ClassMethods, Findable::ClassMethods
+  include Memorable::InstanceMethods, Paramable::InstanceMethods
 
   @@artists = []
-
-  def initialize
-    @@artists << self
-    @songs = []
-  end
 
   def self.find_by_name(name)
     @@artists.detect{|a| a.name == name}
@@ -17,14 +14,6 @@ class Artist
 
   def self.all
     @@artists
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
   end
 
   def add_song(song)
@@ -36,7 +25,4 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
 end
